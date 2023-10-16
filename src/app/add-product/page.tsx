bf0c7a7d5d81b261bb7f1e5dbb545aeb72ad9ff8
Yohan Baes <yohan.baes97@gmail.com>
2023-10-16 14:57:10 +0200

    yes

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,11 +1,63 @@
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+
+export const metadata = {
+    title: "Add Product - Flowmazon"
+}
+
+async function addProduct(formData: FormData) {
+    "use server";
+
+    const name = formData.get('name')?.toString();
+    const description = formData.get('description')?.toString();
+    const imageUrl = formData.get('imageUrl')?.toString();
+    const price = Number(formData.get('price') || 0);
+
+    if (!name || !description || !imageUrl || !price) {
+        throw Error('Missing required fields');
+    }
+
+    await prisma.product.create({
+        data: {name, description, imageUrl, price},
+    })
+    
+    redirect('/');
+}
+
 export default function AddProductPage() {
     return (
         <div className="text-black">
             <h1 className="text-lg mb-3 font-bold">Add Product</h1>
-            <form action="">
+            <form action={addProduct}>
                 <input
-                    className="mb-3 w-full"
+                    required
+                    name="name"
+                    placeholder="Name"
+                    className="mb-3 w-full input input-bordered bg-[#f1f1f1]"
                 />
+                <textarea
+                    name="description" 
+                    required 
+                    placeholder="Description" 
+                    className="textarea textarea-bordered mb-3 w-full bg-[#f1f1f1]" 
+                />
+                <input
+                    required
+                    name="imageUrl"
+                    placeholder="Image URL"
+                    className="mb-3 w-full input input-bordered bg-[#f1f1f1]"
+                    type="url"
+                />
+                <input
+                    required
+                    name="price"
+                    placeholder="Price"
+                    type="number"
+                    className="mb-3 w-full input input-bordered bg-[#f1f1f1]"
+                />
+                <button type="submit" className="btn btn-primary btn-block">
+                    Add Produt
+                </button>
             </form>
         </div>
     )
